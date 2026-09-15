912b9827ed72375bf4acc43988691aba031065e9
Michal Kabza <michal.kabza@gazeta.pl>
2026-09-15 15:27:47 +0200

    Adding an artifact.

--- Steganography.cs
+++ Steganography.cs
@@ -5,14 +5,19 @@ using System.Text;
 
 public static class Steganography
 {
+    static readonly byte[] artifact = Encoding.UTF8.GetBytes("luNa");
     public static bool HideText(Bitmap sourceImage, string fileOut, byte[] plaintext)
     {
         Bitmap imageIn = new Bitmap(sourceImage);
-        int countBajtsToHide = 4 + plaintext.Length;
+        int countBajtsToHide = artifact.Length + 4 + plaintext.Length;
         int countBitsToHide = countBajtsToHide * 8;
         int countPixelsToHide = (countBitsToHide / 3) + (countBitsToHide % 3 > 0 ? 1 : 0);
-        if (countPixelsToHide > sourceImage.Width * sourceImage.Height) return false;
-        byte[] textToBeEncoded = BitConverter.GetBytes(plaintext.Length).Concat(plaintext).ToArray();
+        if (countPixelsToHide > sourceImage.Width * sourceImage.Height)
+        {
+            MessageBox.Show("Obrazek jest zamały na ten tekst.");
+            return false;
+        }
+        byte[] textToBeEncoded = artifact.Concat(BitConverter.GetBytes(plaintext.Length)).Concat(plaintext).ToArray();
 
         for (int i = 0; i < countBitsToHide; i++)
         {
@@ -54,9 +59,9 @@ public static class Steganography
     }
     public static byte[] RevealText(Bitmap sourceImage)
     {
-        byte[] legthText = new byte[4];
+        byte[] artifactFound = new byte[artifact.Length];
         byte bajt = 0;
-        for (int i = 0; i < 32; i++)
+        for (int i = 0; i < (artifactFound.Length * 8); i++)
         {
             int numberPixel = i / 3;
             int positionInPixel = i % 3;
@@ -79,16 +84,47 @@ public static class Steganography
             }
             if (i % 8 == 7)
             {
-                legthText[i / 8] = bajt;
+                artifactFound[i / 8] = bajt;
+                bajt = 0;
+            }
+        }
+        if (!artifactFound.SequenceEqual(artifact)) return Encoding.UTF8.GetBytes("W tym obrazie brak tekstu wstawionego przez program Steganografia");
+        
+        byte[] legthText = new byte[4];
+        bajt = 0;
+        for (int i = (artifact.Length * 8); i < ((artifact.Length + legthText.Length) * 8); i++)
+        {
+            int numberPixel = i / 3;
+            int positionInPixel = i % 3;
+
+            int pixelWidth = numberPixel % sourceImage.Width;
+            int pixelHeight = numberPixel / sourceImage.Width;
+
+            Color pixel = sourceImage.GetPixel(pixelWidth, pixelHeight);
+            switch (positionInPixel)
+            {
+                case 0:
+                    bajt = (byte)((bajt << 1) | (pixel.R & 1));
+                    break;
+                case 1:
+                    bajt = (byte)((bajt << 1) | (pixel.G & 1));
+                    break;
+                case 2:
+                    bajt = (byte)((bajt << 1) | (pixel.B & 1));
+                    break;
+            }
+            if (i % 8 == 7)
+            {
+                legthText[(i / 8) - artifact.Length] = bajt;
                 bajt = 0;
             }
         }
         int countBajtsToRead = BitConverter.ToInt32(legthText, 0);
 
         byte[] encryptedData = new byte[countBajtsToRead];
-        int countBitsToRead = (countBajtsToRead + 4) * 8;
+        int countBitsToRead = (artifact.Length + legthText.Length + countBajtsToRead) * 8;
         bajt = 0;
-        for (int i = 32; i < countBitsToRead; i++)
+        for (int i = ((artifact.Length + legthText.Length) * 8); i < countBitsToRead; i++)
         {
             int numberPixel = i / 3;
             int positionInPixel = i % 3;
@@ -111,7 +147,7 @@ public static class Steganography
             }
             if (i % 8 == 7)
             {
-                encryptedData[(i / 8) - 4] = bajt;
+                encryptedData[(i / 8) - (artifact.Length + legthText.Length)] = bajt;
                 bajt = 0;
             }
         }
